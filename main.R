2252174e5a736cde448e8ff792a46340c6218918
Ilyess Rachedi <rachedi@seneca.embl.de>
2020-02-20 16:42:00 +0100

    Add main file and data

---
+++ main.R
@@ -0,0 +1,30 @@
+source("AllClasses.R")
+source("AllGenerics.R")
+source("methods-accessor-scRNAseq.R")
+source("methods-replace-scRNAseq.R")
+source("methods-class-scRNAseq.R")
+
+## Data
+outputDirectory <- "./YourOutputDirectory"
+experimentName <- "Bergiers"
+
+countMatrix <- read.delim(file.path(system.file("extdata", package="conclus"),		
+                                    "Bergiers_counts_matrix_filtered.tsv"), 
+                          stringsAsFactors = FALSE)
+
+columnsMetaData <- read.delim(file.path(system.file("extdata",
+                                                    package="conclus"), 
+                                        "Bergiers_colData_filtered.tsv"))
+
+## Construction
+scr <- scRNAseq(experimentName  = experimentName, 
+                countMatrix     = countMatrix, 
+                colData         = columnsMetaData,
+                species         = "mmu",
+                outputDirectory = outputDirectory 
+)
+
+## Normalization with S4 method
+scrS4 <- normaliseCountMatrix(src, colData = columnsMetaData) # return NCM
+getNormalizedCountMatrix(scr)    # Slot normalizedCountMatrix empty
+getNormalizedCountMatrix(scrS4)  # Slot normalizedCountMatrix full
