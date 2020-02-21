3b6916a2ad148ca27a9073f0425c7c7edf494054
Ilyess Rachedi <rachedi@seneca.embl.de>
2020-02-21 12:29:17 +0100

    Add packages loading

--- main.R
+++ main.R
@@ -1,8 +1,20 @@
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(devtools,BiocManager, ggplot2, pheatmap, zoo, dynamicTreeCut, factoextra, 
+               digest, RColorBrewer, doParallel, BiocParallel, scran, 
+               monocle, KEGGREST, AnnotationDbi, SingleCellExperiment, Cairo, rvest, curl, 
+               scater, Matrix, dbscan, fpc, matrixStats, dplyr, biomaRt, org.Mm.eg.db,
+               grDevices, S4Vectors, Biobase, DataCombine, zoo, rvest)
+
+library(devtools)
+library(BiocManager)
+
 source("R/AllClasses.R")
 source("R/AllGenerics.R")
 source("R/methods-accessor-scRNAseq.R")
 source("R/methods-replace-scRNAseq.R")
 source("R/methods-class-scRNAseq.R")
+
 
 
 ## Data
@@ -19,8 +31,7 @@ scr <- scRNAseq(experimentName  = experimentName,
                 countMatrix     = countMatrix, 
                 colData         = columnsMetaData,
                 species         = "mmu",
-                outputDirectory = outputDirectory 
-)
+                outputDirectory = outputDirectory)
 
 ## Normalization with S4 method
 scrS4 <- normaliseCountMatrix(scr, colData = columnsMetaData) # return NCM
