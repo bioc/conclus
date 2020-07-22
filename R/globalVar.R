38c390981e9b30b289059fc06fbaffca9b1c55e9
Nicolas Descostes <nicolas.descostes@embl.it>
2020-07-22 16:09:38 +0200

    update

--- R/globalVar.R
+++ R/globalVar.R
@@ -1,1 +1,9 @@
-utils::globalVariables(c("PCAGetTSNEresults", "perpGetTSNEresults"))
+utils::globalVariables(c(
+				## function .getTSNEresults in foreach
+				"PCAGetTSNEresults", "perpGetTSNEresults",
+				
+				## function .mkDbscan in foreach
+				"iMkDbscan", "epsMkDbscan", "MinPtsMkDbscan",
+				
+				## function .mkSimMat in foreach
+				"iMkSimMat"))
