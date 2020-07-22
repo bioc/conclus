d48423b44a575546e4335c6db55dda02cfbfb4ce
Nicolas Descostes <nicolas.descostes@embl.it>
2020-07-22 16:24:04 +0200

    add variables for biomart

--- R/globalVar.R
+++ R/globalVar.R
@@ -6,4 +6,10 @@ utils::globalVariables(c(
 				"iMkDbscan", "epsMkDbscan", "MinPtsMkDbscan",
 				
 				## function .mkSimMat in foreach
-				"iMkSimMat"))
+				"iMkSimMat",
+				
+				## function retrieveGenesInfo from dplyr operations
+				"uniprot_gn_symbol", "chromosome_name", 
+				"entrezgene_description", "go_id", "uniprot_gn_id", 
+				"description", "external_gene_name", "gene_biotype",
+				"ensembl_gene_id", "entrezgene_id"))
