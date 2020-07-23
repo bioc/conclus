817ce2a1246ca051949597dffc4e6ca1455bb57d
Nicolas Descostes <nicolas.descostes@embl.it>
2020-07-23 10:34:07 +0200

    initial commit

---
+++ R/constructors.R
@@ -0,0 +1,39 @@
+#' @description
+#' Constructor of the class scRNAseq (might be extended in the future).
+#'   
+#' @rdname constructors
+#' @name constructors
+#' @title constructors
+NULL
+
+
+#' @usage
+#' getExperimentName(theObject)
+#' 
+#' @description
+#' getExperimentName: Get the name of the experiment.
+#' 
+#' @param experimentName 'character' string representing the name of the 
+#' experiment. Many output of scRNAseq will use this name.
+#' @param countMatrix An 'integer matrix' representing the raw count matrix with 
+#' reads or unique molecular identifiers (UMIs).
+#' @param species 'character' string representing the species of interest. 
+#' Currently limited to "mouse" and "human".
+#' @param outputDirectory A 'character' string of the path to the root output 
+#' folder.
+#' 
+#' @rdname constructors
+#' @aliases scRNAseq
+#' @seealso scRNAseq-class
+#' @export scRNAseq
+scRNAseq <- function(experimentName, countMatrix, species, outputDirectory){
+	
+	new("scRNAseq",
+			experimentName=experimentName, 
+			countMatrix=countMatrix, 
+			species=species, 
+			outputDirectory=outputDirectory)
+}
+
+
+
