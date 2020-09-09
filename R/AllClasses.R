338dadf46543c5f781cfa11d8db56355bf2bb2c6
Nicolas Descostes <nicolas.descostes@embl.it>
2020-09-09 11:53:33 +0200

    reduce validity function

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -149,6 +149,172 @@ Dbscan <- setClass(
 ############################## scRNAseq class ##################################
 ################################################################################
 
+
+.testExperimentNameSlot <- function(object){
+	
+	experimentName <- getExperimentName(object)
+	
+	if(isTRUE(all.equal(length(experimentName),0)) ||
+			isTRUE(all.equal(experimentName,"")))
+		stop("'experimentName' slot is empty. Please fill it.")
+	
+	if(!is.character(experimentName) | grepl(" ", experimentName))
+		stop("Experiment name should contain a single string ",
+				"describing the experiment, '", experimentName,
+				"' is not correct.")
+}
+
+
+.testCountMatrixSlot <- function(object){
+	
+	countMatrix <- getCountMatrix(object)
+	
+	if(isTRUE(all.equal(nrow(countMatrix), 0)) &&
+			isTRUE(all.equal(ncol(countMatrix), 0)))
+		stop("The count matrix should not be empty. Please fill it.")
+	
+	if(all(is.na(countMatrix)))
+		stop("'countMatrix' slot is empty. It should be a matrix ",
+				"containing at leat 100 cells.\n")
+	
+	if(ncol(countMatrix) < 100)
+		stop("Not enough cells in the count matrix. There ",
+				"Should be at leat 100 cells. ", "The current count matrix ",
+				"contains ", ncol(countMatrix), " cells.\n")
+	
+}
+
+.testsceNormSlot <- function(object){
+	
+	sceNorm <- getSceNorm(object)
+	
+	if(!is(sceNorm, "SingleCellExperiment"))
+		stop("Normalized count matrix should be a SingleCellExperiment ",
+				"object and not a '", is(sceNorm), "'.")
+}
+
+.testSpeciesSlot <- function(object){
+	
+	species <- getSpecies(object)
+	
+	if(isTRUE(all.equal(length(species), 0)))
+		stop("The species is empty. Please fill it.")
+	
+	if (!is.element(species, c("mouse","human")))
+		stop("species should be 'mouse' or 'human'. '", species,
+				"' is currently not supported.\n")
+	
+}
+
+.testOutputDirectorySlot <- function(object){
+	
+	outputDirectory <- getOutputDirectory(object)
+	
+	if (isTRUE(all.equal(length(outputDirectory), 0)) ||
+			isTRUE(all.equal(outputDirectory, "")))
+		stop("'outputDirectory' slot is empty. Please fill it.")
+	
+	if(!is.character(outputDirectory) | grepl(" ", outputDirectory))
+		stop("'outputDirectory' should be a conform folder path:",
+				"'", outputDirectory, "' is not.")
+}
+
+
+.testtSNEListSlot <- function(object){
+	
+	tSNEList <- getTSNEList(object)
+	
+	if(isTRUE(all.equal(length(tSNEList), 0)))
+		stop("tSNEList is empty. This should be a list of tSNE objects.\n")
+	
+	invisible(checkList(tSNEList, getCoordinates, "Tsne"))
+}
+
+.testDbscanSlot <- function(object){
+	
+	dbscanList <- getDbscanList(object)
+	
+	if(isTRUE(all.equal(length(dbscanList), 0)))
+		stop("dbscanList is empty. This should be a list of dbScan ",
+				"objects.\n")
+	
+	invisible(checkList(dbscanList, getClustering, "Dbscan"))
+}
+
+.testCellsSimilarityMatrixSlot <- function(object){
+	
+	cellsSimilarityMatrix <- getCellsSimilarityMatrix(object)
+	
+	if(!isTRUE(all.equal(nrow(cellsSimilarityMatrix),
+					ncol(cellsSimilarityMatrix))))
+		stop("'cellsSimilarityMatrix' slot should contain a square matrix.")
+}
+
+.testClustersSimilarityMatrixSlot <- function(object){
+	
+	clustersSimilarityMatrix <- getClustersSimilarityMatrix(object)
+	
+	if (!isTRUE(all.equal(nrow(clustersSimilarityMatrix),
+					ncol(clustersSimilarityMatrix))))
+		stop("'clustersSimilarityMatrix' slot should contain a square ",
+				"matrix. ")
+}
+
+.testClustersSimiliratyOrderedSlot <- function(object){
+	
+	clustersSimiliratyOrdered <- getClustersSimiliratyOrdered(object)
+	clustersSimilarityMatrix <- getClustersSimilarityMatrix(object)
+	
+	if(isTRUE(all.equal(nrow(clustersSimiliratyOrdered), 0)) &&
+			isTRUE(all.equal(ncol(clustersSimiliratyOrdered), 0)))
+		stop("'clustersSimiliratyOrdered' is empty. It should be a matrix.")
+	
+	if (all(!clustersSimiliratyOrdered %in%
+					rownames(clustersSimilarityMatrix)))
+		stop("'clustersSimiliratyOrdered' slot should contain the same ",
+				"clusters as 'clustersSimilarityMatrix'.")
+}
+
+.testGetMarkerGenesListSlot <- function(object){
+	
+	markerGenesList <- getMarkerGenesList(object)
+	clustersSimiliratyOrdered <- getClustersSimiliratyOrdered(object)
+	
+	if(isTRUE(all.equal(length(markerGenesList), 0)))
+		stop("markerGenesList is empty. This should be a list of dataframe")
+	
+	invisible(checkMarkerGenesList(markerGenesList,
+					clustersSimiliratyOrdered))
+}
+
+
+.testClustersMarkersSlot <- function(object){
+	
+	clusterMarkers <- getClustersMarkers(object)
+	clustersSimiliratyOrdered <- getClustersSimiliratyOrdered(object)
+	
+	if(isTRUE(all.equal(length(clusterMarkers), 0)))
+		stop("clusterMarkers is empty. This should be a dataframe")
+	
+	invisible(checkClusterMarkers(clusterMarkers,
+					clustersSimiliratyOrdered))
+}
+
+
+.testGenesInfosSlot <- function(object){
+	
+	genesInfos <- getGenesInfos(object)
+	clustersSimiliratyOrdered <- getClustersSimiliratyOrdered(object)
+	
+	if(isTRUE(all.equal(length(genesInfos), 0)))
+		stop("genesInfos is empty. This should be a dataframe")
+	
+	invisible(checkGenesInfos(genesInfos, species,
+					clustersSimiliratyOrdered))
+	
+}
+
+
 #' The scRNAseq class
 #'
 #' @description
@@ -286,137 +452,18 @@ scRNAseq <- setClass(
 
     validity = function(object) {
 
-        ## Test experimentName slot
-        experimentName <- getExperimentName(object)
-
-        if(isTRUE(all.equal(length(experimentName),0)) ||
-                isTRUE(all.equal(experimentName,"")))
-            stop("'experimentName' slot is empty. Please fill it.")
-
-        if(!is.character(experimentName) | grepl(" ", experimentName))
-            stop("Experiment name should contain a single string ",
-                "describing the experiment, '", experimentName,
-                "' is not correct.")
-
-        ## Test countMatrix slot
-        countMatrix <- getCountMatrix(object)
-
-        if(isTRUE(all.equal(nrow(countMatrix), 0)) &&
-                isTRUE(all.equal(ncol(countMatrix), 0)))
-            stop("The count matrix should not be empty. Please fill it.")
-
-        if(all(is.na(countMatrix)))
-            stop("'countMatrix' slot is empty. It should be a matrix ",
-                    "containing at leat 100 cells.\n")
-
-        if(ncol(countMatrix) < 100)
-            stop("Not enough cells in the count matrix. There ",
-                "Should be at leat 100 cells. ", "The current count matrix ",
-                "contains ", ncol(countMatrix), " cells.\n")
-
-
-        ## Test sceNorm slot
-        sceNorm <- getSceNorm(object)
-
-        if(!is(sceNorm, "SingleCellExperiment"))
-            stop("Normalized count matrix should be a SingleCellExperiment ",
-                    "object and not a '", is(sceNorm), "'.")
-
-        ## Test species slot
-        species <- getSpecies(object)
-
-        if(isTRUE(all.equal(length(species), 0)))
-            stop("The species is empty. Please fill it.")
-
-        if (!is.element(species, c("mouse","human")))
-            stop("species should be 'mouse' or 'human'. '", species,
-                    "' is currently not supported.\n")
-
-        ## Test outputDirectory slot
-        outputDirectory <- getOutputDirectory(object)
-
-        if (isTRUE(all.equal(length(outputDirectory), 0)) ||
-                isTRUE(all.equal(outputDirectory, "")))
-            stop("'outputDirectory' slot is empty. Please fill it.")
-
-        if(!is.character(outputDirectory) | grepl(" ", outputDirectory))
-            stop("'outputDirectory' should be a conform folder path:",
-                "'", outputDirectory, "' is not.")
-
-        ## Test tSNEList slot
-        tSNEList <- getTSNEList(object)
-
-        if(isTRUE(all.equal(length(tSNEList), 0)))
-            stop("tSNEList is empty. This should be a list of tSNE objects.\n")
-
-        invisible(checkList(tSNEList, getCoordinates, "Tsne"))
-
-
-        ## Test dbscan slot
-        dbscanList <- getDbscanList(object)
-
-        if(isTRUE(all.equal(length(dbscanList), 0)))
-            stop("dbscanList is empty. This should be a list of dbScan ",
-                    "objects.\n")
-
-        invisible(checkList(dbscanList, getClustering, "Dbscan"))
-
-
-        ## Test cellsSimilarityMatrix slot
-        cellsSimilarityMatrix <- getCellsSimilarityMatrix(object)
-
-        if(!isTRUE(all.equal(nrow(cellsSimilarityMatrix),
-                        ncol(cellsSimilarityMatrix))))
-            stop("'cellsSimilarityMatrix' slot should contain a square matrix.")
-
-
-        ## Test clustersSimilarityMatrix slot
-        clustersSimilarityMatrix <- getClustersSimilarityMatrix(object)
-
-        if (!isTRUE(all.equal(nrow(clustersSimilarityMatrix),
-                        ncol(clustersSimilarityMatrix))))
-            stop("'clustersSimilarityMatrix' slot should contain a square ",
-                    "matrix. ")
-
-
-        ## Test clustersSimiliratyOrdered slot
-        clustersSimiliratyOrdered <- getClustersSimiliratyOrdered(object)
-
-        if(isTRUE(all.equal(nrow(clustersSimiliratyOrdered), 0)) &&
-                isTRUE(all.equal(ncol(clustersSimiliratyOrdered), 0)))
-            stop("'clustersSimiliratyOrdered' is empty. It should be a matrix.")
-
-        if (all(!clustersSimiliratyOrdered %in%
-                        rownames(clustersSimilarityMatrix)))
-            stop("'clustersSimiliratyOrdered' slot should contain the same ",
-                    "clusters as 'clustersSimilarityMatrix'.")
-
-
-        ## Test getMarkerGenesList slot
-        markerGenesList <- getMarkerGenesList(object)
-
-        if(isTRUE(all.equal(length(markerGenesList), 0)))
-            stop("markerGenesList is empty. This should be a list of dataframe")
-
-        invisible(checkMarkerGenesList(markerGenesList,
-                        clustersSimiliratyOrdered))
-
-        ## Test clustersMarkers
-        clusterMarkers <- getClustersMarkers(object)
-
-        if(isTRUE(all.equal(length(clusterMarkers), 0)))
-            stop("clusterMarkers is empty. This should be a dataframe")
-
-        invisible(checkClusterMarkers(clusterMarkers,
-                        clustersSimiliratyOrdered))
-
-        ## Test genesInfos slot
-        genesInfos <- getGenesInfos(object)
-
-        if(isTRUE(all.equal(length(genesInfos), 0)))
-            stop("genesInfos is empty. This should be a dataframe")
-
-        invisible(checkGenesInfos(genesInfos, species,
-                        clustersSimiliratyOrdered))
-    }
+		.testExperimentNameSlot(object)
+		.testCountMatrixSlot(object)
+		.testsceNormSlot(object)
+		.testSpeciesSlot(object)
+		.testOutputDirectorySlot(object)
+		.testtSNEListSlot(object)
+		.testDbscanSlot(object)
+		.testCellsSimilarityMatrixSlot(object)
+		.testClustersSimilarityMatrixSlot(object)
+		.testClustersSimiliratyOrderedSlot(object)
+		.testGetMarkerGenesListSlot(object)        
+		.testClustersMarkersSlot(object)        
+		.testGenesInfosSlot(object)
+	}
 )
