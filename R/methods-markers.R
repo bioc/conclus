acf87b68ca8922516544ba3882cde9f659a03178
Nicolas Descostes <nicolas.descostes@embl.it>
2020-09-11 17:08:06 +0200

    replace tabs by spaces

--- R/methods-markers.R
+++ R/methods-markers.R
@@ -45,42 +45,42 @@
 #' t-test, the mean log 10 FDR, and the score.
 #' @noRd
 .buildTTestFDR <- function(mat, allGroups, currentGroup, tTestPval, 
-		currentSimMed, simMedNames){
-	
-	tTestFDR <- data.frame(row.names=rownames(mat))
-	otherGroups <- allGroups[allGroups!=currentGroup]
-	
-	tTestFDR <- lapply(otherGroups, function(currentOther){
-				
-				tTestFDR[, paste0("vs_", currentOther)] <-
-						p.adjust(tTestPval[, paste0("vs_",
-												currentOther)],
-								method="fdr")
-				return(tTestFDR)
-			})
-	
-	tTestFDR <- dplyr::bind_cols(tTestFDR)
-	tTestFDR <- cbind(Gene=as.factor(rownames(mat)), tTestFDR)
-	
-	submat <- as.matrix(tTestFDR[, 2:(length(otherGroups) + 1 )])
-	
-	## Add column mean_log10_fdr
-	tTestFDR$mean_log10_fdr <- rowMeans(log10(submat + 1e-300))
-	
-	## Add column n_05
-	tTestFDR$n_05 <- apply(submat, 1, function(x)
-				length(x[!is.na(x) & x < 0.05]))
-	
-	##Add column score
-	weights <- currentSimMed[match(otherGroups, simMedNames)]
-	tTestFDR$score <- apply(submat, 1, function(x)
-				sum(-log10(x+1e-300) * weights) / ncol(submat))
-	
-	tTestFDR <- tTestFDR[order(tTestFDR$score, decreasing=TRUE), ]
-	row.names(tTestFDR) <- NULL
-	
-	return(tTestFDR)
-	
+        currentSimMed, simMedNames){
+    
+    tTestFDR <- data.frame(row.names=rownames(mat))
+    otherGroups <- allGroups[allGroups!=currentGroup]
+    
+    tTestFDR <- lapply(otherGroups, function(currentOther){
+                
+                tTestFDR[, paste0("vs_", currentOther)] <-
+                        p.adjust(tTestPval[, paste0("vs_",
+                                                currentOther)],
+                                method="fdr")
+                return(tTestFDR)
+            })
+    
+    tTestFDR <- dplyr::bind_cols(tTestFDR)
+    tTestFDR <- cbind(Gene=as.factor(rownames(mat)), tTestFDR)
+    
+    submat <- as.matrix(tTestFDR[, 2:(length(otherGroups) + 1 )])
+    
+    ## Add column mean_log10_fdr
+    tTestFDR$mean_log10_fdr <- rowMeans(log10(submat + 1e-300))
+    
+    ## Add column n_05
+    tTestFDR$n_05 <- apply(submat, 1, function(x)
+                length(x[!is.na(x) & x < 0.05]))
+    
+    ##Add column score
+    weights <- currentSimMed[match(otherGroups, simMedNames)]
+    tTestFDR$score <- apply(submat, 1, function(x)
+                sum(-log10(x+1e-300) * weights) / ncol(submat))
+    
+    tTestFDR <- tTestFDR[order(tTestFDR$score, decreasing=TRUE), ]
+    row.names(tTestFDR) <- NULL
+    
+    return(tTestFDR)
+    
 }
 
 
@@ -105,7 +105,7 @@
 .buildTTestPval <- function(otherGroups, tTestPval, colDF, mat, colLabel,
         currentGroup){
 
-	tTestPval <- data.frame(row.names=rownames(mat))
+    tTestPval <- data.frame(row.names=rownames(mat))
 
     tTestPval <- lapply(otherGroups, function(currentOther){
 
@@ -120,8 +120,8 @@
                 return(tTestPval)
             })
 
-	tTestPval <- dplyr::bind_cols(tTestPval)
-	tTestPval <- cbind(Gene=rownames(mat), tTestPval)
+    tTestPval <- dplyr::bind_cols(tTestPval)
+    tTestPval <- cbind(Gene=rownames(mat), tTestPval)
 
     return(tTestPval)
 }
@@ -168,10 +168,10 @@
                         colLabel, currentGroup)
                 
                 ## Apply the FDR
-				tTestFDR <- .buildTTestFDR(mat, allGroups, currentGroup, 
-						tTestPval, currentSimMed, simMedNames)
-				
-				## Write list if option = TRUE
+                tTestFDR <- .buildTTestFDR(mat, allGroups, currentGroup, 
+                        tTestPval, currentSimMed, simMedNames)
+                
+                ## Write list if option = TRUE
                 if(writeMarkerGenes){
 
                     outputDir <- file.path(getOutputDirectory(theObject),
@@ -477,32 +477,32 @@ setMethod(
 #' @importFrom biomaRt getBM
 #' @return Return the information described above..
 .queryBiomart <- function(genes, ensembl){
-	
-	database <- merge(x = .returnDB1(genes, ensembl),
-			y = .returnDB2(genes, ensembl),
-			by = c("uniprot_gn_symbol", "external_gene_name"),
-			all.x = TRUE)
-	
-	## Group the GO id and the uniprot Id
-	options(dplyr.summarise.inform = FALSE)
-	database <- database %>% group_by(uniprot_gn_symbol,
-					chromosome_name, entrezgene_description) %>%
-			summarise(go_id = paste(unique(go_id), collapse=', '),
-					uniprot_gn_id = paste(unique(uniprot_gn_id),
-							collapse=', '),
-					description = paste(unique(description),
-							collapse=', '),
-					external_gene_name = paste(
-							unique(external_gene_name), collapse=', '),
-					gene_biotype = paste(unique(gene_biotype),
-							collapse=', '),
-					ensembl_gene_id = paste(unique(ensembl_gene_id),
-							collapse=', '),
-					entrezgene_id = paste(unique(entrezgene_id),
-							collapse=', '),
-			)
-	
-	return(database)
+    
+    database <- merge(x = .returnDB1(genes, ensembl),
+            y = .returnDB2(genes, ensembl),
+            by = c("uniprot_gn_symbol", "external_gene_name"),
+            all.x = TRUE)
+    
+    ## Group the GO id and the uniprot Id
+    options(dplyr.summarise.inform = FALSE)
+    database <- database %>% group_by(uniprot_gn_symbol,
+                    chromosome_name, entrezgene_description) %>%
+            summarise(go_id = paste(unique(go_id), collapse=', '),
+                    uniprot_gn_id = paste(unique(uniprot_gn_id),
+                            collapse=', '),
+                    description = paste(unique(description),
+                            collapse=', '),
+                    external_gene_name = paste(
+                            unique(external_gene_name), collapse=', '),
+                    gene_biotype = paste(unique(gene_biotype),
+                            collapse=', '),
+                    ensembl_gene_id = paste(unique(ensembl_gene_id),
+                            collapse=', '),
+                    entrezgene_id = paste(unique(entrezgene_id),
+                            collapse=', '),
+            )
+    
+    return(database)
 }
 
 
@@ -526,19 +526,19 @@ setMethod(
 #' @return Return the uniprot gene symbol, the external gene name, and the
 #' mgi information if the species is mouse.
 .groupGOandUniprotID <- function(speDbID, speDBdescription, genes, ensembl){
-	
-	if(!(is.null(speDbID) & is.null(speDBdescription)))
-		## Query biomart for specific db according to species
-		## external_gene_name is also searched because one gene can have
-		## several external_gene_name and so several speDbID and
-		## speDBdescription
-		spDB <- getBM(attributes=c("uniprot_gn_symbol",  # geneName
-						"external_gene_name", speDbID,
-						speDBdescription),
-				values=genes$geneName,
-				filters = "uniprot_gn_symbol",
-				mart=ensembl)
-	return(spDB)
+    
+    if(!(is.null(speDbID) & is.null(speDBdescription)))
+        ## Query biomart for specific db according to species
+        ## external_gene_name is also searched because one gene can have
+        ## several external_gene_name and so several speDbID and
+        ## speDBdescription
+        spDB <- getBM(attributes=c("uniprot_gn_symbol",  # geneName
+                        "external_gene_name", speDbID,
+                        speDBdescription),
+                values=genes$geneName,
+                filters = "uniprot_gn_symbol",
+                mart=ensembl)
+    return(spDB)
 }
 
 
@@ -562,37 +562,37 @@ setMethod(
 #' @importFrom biomaRt useEnsembl
 #' @return A database with biomaRt information.
 .defineDatabase <- function(species, genes, speDbID, speDBdescription){
-	
-	## Selecting the BioMart database to use
-	databaseDict <- c(mouse = "mmusculus_gene_ensembl",
-			human = "hsapiens_gene_ensembl")
-	dataset <- databaseDict[species]
-	ensembl <- useEnsembl(biomart="genes", dataset=dataset)
-	
-	## Query biomart
-	database <- .queryBiomart(genes, ensembl)
-	
-	## Group the GO id and the uniprot Id
-	spDB <- .groupGOandUniprotID(speDbID, speDBdescription, genes, 
-			ensembl)
-	
-	## Merge the second column, the first already existing in the first
-	## table
-	if(!is.null(spDB))
-		database <- merge(x = database, y = spDB, by =
-						c("uniprot_gn_symbol", "external_gene_name"),
-				all.x = TRUE)
-	
-	## Add cluster column
-	database <- merge(x = database, y = genes,
-			by.x = "uniprot_gn_symbol",  by.y = "geneName",
-			all.x = TRUE)
-	
-	## Add Symbol column
-	database <- cbind(database, Symbol = database$uniprot_gn_symbol)
-	
-	return(database)
-	
+    
+    ## Selecting the BioMart database to use
+    databaseDict <- c(mouse = "mmusculus_gene_ensembl",
+            human = "hsapiens_gene_ensembl")
+    dataset <- databaseDict[species]
+    ensembl <- useEnsembl(biomart="genes", dataset=dataset)
+    
+    ## Query biomart
+    database <- .queryBiomart(genes, ensembl)
+    
+    ## Group the GO id and the uniprot Id
+    spDB <- .groupGOandUniprotID(speDbID, speDBdescription, genes, 
+            ensembl)
+    
+    ## Merge the second column, the first already existing in the first
+    ## table
+    if(!is.null(spDB))
+        database <- merge(x = database, y = spDB, by =
+                        c("uniprot_gn_symbol", "external_gene_name"),
+                all.x = TRUE)
+    
+    ## Add cluster column
+    database <- merge(x = database, y = genes,
+            by.x = "uniprot_gn_symbol",  by.y = "geneName",
+            all.x = TRUE)
+    
+    ## Add Symbol column
+    database <- cbind(database, Symbol = database$uniprot_gn_symbol)
+    
+    return(database)
+    
 }
 
 #' .orderCol
@@ -615,24 +615,24 @@ setMethod(
 #'
 #' @return The re-ordered database if orderGenes is set to 'initial'.
 .orderCol <- function(speDBdescription, speDbID, database, orderGenes, genes){
-	
-	colnamesOrder <- c("uniprot_gn_symbol", "clusters",
-			"external_gene_name", "go_id", speDBdescription,
-			"entrezgene_description", "gene_biotype", "chromosome_name",
-			"Symbol", "ensembl_gene_id", speDbID, "entrezgene_id",
-			"uniprot_gn_id")
-	
-	result  <- database[, colnamesOrder]
-	
-	if(isTRUE(all.equal(orderGenes, "initial"))){
-		desiredOrder <- genes$geneName
-		result <- merge(list(uniprot_gn_symbol = unique(desiredOrder)),
-				result, sort = FALSE)
-		rownames(result) <- seq_len(nrow(result))
-	}
-	
-	return(result)
-	
+    
+    colnamesOrder <- c("uniprot_gn_symbol", "clusters",
+            "external_gene_name", "go_id", speDBdescription,
+            "entrezgene_description", "gene_biotype", "chromosome_name",
+            "Symbol", "ensembl_gene_id", speDbID, "entrezgene_id",
+            "uniprot_gn_id")
+    
+    result  <- database[, colnamesOrder]
+    
+    if(isTRUE(all.equal(orderGenes, "initial"))){
+        desiredOrder <- genes$geneName
+        result <- merge(list(uniprot_gn_symbol = unique(desiredOrder)),
+                result, sort = FALSE)
+        rownames(result) <- seq_len(nrow(result))
+    }
+    
+    return(result)
+    
 }
 
 
@@ -760,13 +760,13 @@ setMethod(
                 speDbID <- speDBdescription <- spDB <- NULL
                 
             
-			database <- .defineDatabase(species, genes, speDbID, 
-					speDBdescription)
-						
+            database <- .defineDatabase(species, genes, speDbID, 
+                    speDBdescription)
+                        
             ## Order the data frames colums to be abe to bind them
-			result <- .orderCol(speDBdescription, speDbID, database, orderGenes,
-					genes)
-			
+            result <- .orderCol(speDBdescription, speDbID, database, orderGenes,
+                    genes)
+            
             rm(database)
             biomartCacheClear()
 
