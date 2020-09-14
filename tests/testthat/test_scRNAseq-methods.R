2b482ec6439a9ed6c087176b42ce615499fd57bf
Nicolas Descostes <nicolas.descostes@embl.it>
2020-09-14 10:11:25 +0200

    Delete comments

--- tests/testthat/test_scRNAseq-methods.R
+++ tests/testthat/test_scRNAseq-methods.R
@@ -64,7 +64,6 @@ clustWrongcells <- clustAddTab
 clustWrongcells$cells <- paste0("test", clustWrongcells$cells)
 
 ## Load expected results
-## !!! Move to a data folder
 load(file = system.file("extdata/scrLight.Rdat", package="conclus"))
 load(file = system.file("extdata/expected_normalizedMatrix.Rdat", 
                 package="conclus"))
