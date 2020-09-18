bff9fa4cbf47b2e91594b27b9f8736a1ca69d8ac
Nicolas Descostes <nicolas.descostes@embl.it>
2020-09-18 10:58:50 +0200

    merged with IlyessAugust2020

--- tests/testthat/test_getters.R
+++ tests/testthat/test_getters.R
@@ -1,76 +1,170 @@
-# source("R/AllClasses.R")
-# source("R/AllGenerics.R")
-# source("R/getters.R")
-# library(testthat)
+library(conclus)
+## Data
 
-# scr = scRNAseq()
-# tsne = Tsne()
-# dbscan = Dbscan()
+outputDirectory <- "YourOutputDirectory"
+experimentName <- "Bergiers"
+columnsMetaData <- read.delim(
+    system.file("extdata/Bergiers_colData_filtered.tsv",
+        package="conclus"))
+
+## Creation of the count Matrix
+
+countMatrix <- as.matrix(read.delim(
+    system.file("extdata/test_countMatrix.tsv",
+        package="conclus")))
+
+smallMatrix <- countMatrix[, 1:50]
+
+## Retrieve the clustering to add
+clustAddTab <- read.delim(
+    system.file("extdata/Bergiers_clusters_table.tsv", package="conclus"))
+clustAddTabColThree <- cbind(clustAddTab, mock=rep(1, nrow(clustAddTab)))
+clustWrongName <- clustAddTab
+colnames(clustWrongName) <- c("test", "test")
+clustWrongcells <- clustAddTab
+clustWrongcells$cells <- paste0("test", clustWrongcells$cells)
+
+## Load expected results
+## !!! Move to a data folder
+load(file = system.file("extdata/scrLight.Rdat", package="conclus"))
+load(file = system.file("extdata/expected_normalizedMatrix.Rdat",
+    package="conclus"))
+
+
+## Construction of the object
+
+scr <- singlecellRNAseq(experimentName = experimentName,
+    countMatrix     = countMatrix ,
+    species         = "mouse",
+    outputDirectory = outputDirectory)
+
 
 ############################ scRNAseq getters ##################################
 
-# test_that("getExperimentName works properly", {
-#     expect_equal(getExperimentName(scr), character(0))
-# })
-# 
-# test_that("getCountMatrix works properly", {
-#     expect_equivalent(getCountMatrix(scr), matrix(nrow = 0, ncol = 0))
-# })
-# 
-# test_that("getSceNorm works properly", {
-#     expect_equal(getSceNorm(scr), SingleCellExperiment())
-# })
-# 
-# test_that("getSpecies works properly", {
-#     expect_equal(getSpecies(scr), character(0))
-# })
-# 
-# test_that("getOutputDirectory works properly", {
-#     expect_equal(getOutputDirectory(scr), character(0))
-# })
-# 
-# test_that("getTSNEList works properly", {
-#     expect_equal(getTSNEList(scr), list(new("Tsne")))
-# })
-# 
-# test_that("getDbscanList works properly", {
-#     expect_equal(getDbscanList(scr), list(new("Dbscan")))
-# })
-# 
-# 
-# ############################### Tsne getters ###################################
-# 
-# test_that("getName works properly", {
-#     expect_equal(getName(tsne), character(0))
-# })
-# 
-# test_that("getPerplexity works properly", {
-#     expect_equal(getPerplexity(tsne), numeric(0))
-# })
-# 
-# test_that("getDbscanList works properly", {
-#     expect_equal(getPC(tsne), numeric(0))
-# })
-# 
-# test_that("getCoordinates works properly", {
-#     expect_equivalent(getCoordinates(tsne),  matrix(nrow = 0, ncol = 0))
-# })
-# 
-# 
-# 
+test_that("getExperimentName works properly", {
+    expect_equal(getExperimentName(scr), "Bergiers")
+})
+
+test_that("getCountMatrix works properly", {
+    expect_equivalent(getCountMatrix(scr), countMatrix)
+})
+
+test_that("getSpecies works properly", {
+    expect_equal(getSpecies(scr), "mouse")
+})
+
+test_that("getOutputDirectory works properly", {
+    expect_equal(getOutputDirectory(scr), outputDirectory)
+})
+
+test_that("getSceNorm works properly", {
+    expect_equal(getSceNorm(scr), SingleCellExperiment())
+})
+
+test_that("getTSNEList works properly", {
+    expect_equal(getTSNEList(scr), list(new("Tsne")))
+})
+
+test_that("getDbscanList works properly", {
+    expect_equal(getDbscanList(scr), list(new("Dbscan")))
+})
+
+test_that("getCellsSimilarityMatrix works properly", {
+    mat <- matrix(nrow = 1, ncol = 1, dimnames = list("c1", "c1"), data = 1)
+    expect_equal(getCellsSimilarityMatrix(scr), mat)
+})
+
+test_that("getClustersSimilarityMatrix works properly", {
+    mat <- matrix(nrow = 1, ncol = 1, dimnames = list("1", "1"), data = 1)
+    expect_equal(getClustersSimilarityMatrix(scr), mat)
+})
+
+test_that("getClustersSimiliratyOrdered works properly", {
+    expect_equal(getClustersSimiliratyOrdered(scr), factor(1))
+})
+
+test_that("getMarkerGenesList works properly", {
+    l <- list(data.frame(Gene = c("gene1"), mean_log10_fdr = c(NA),
+                            n_05 = c(NA), score = c(NA)))
+    expect_equal(getMarkerGenesList(scr), l)
+})
+
+test_that("getClustersMarkers works properly", {
+    df <- data.frame(geneName="gene1", clusters=NA)
+    expect_equal(getClustersMarkers(scr), df)
+})
+
+test_that("getGenesInfos works properly", {
+    df <- data.frame(uniprot_gn_symbol=c("symbol"), clusters="1",
+        external_gene_name="gene", go_id="GO1,GO2",
+        mgi_description="description", entrezgene_description="descr",
+        gene_biotype="gene", chromosome_name="1", Symbol="symbol",
+        ensembl_gene_id="ENS", mgi_id="MGI", entrezgene_id="1",
+        uniprot_gn_id="ID")
+    expect_equal(getGenesInfos(scr), df)
+})
+
+############################### Tsne getters ###################################
+
+name="Bergiers_tsne_coordinates_1_4PCs_30perp"
+pc=4
+perplexity=30
+coordinates=matrix(data=c(1,2), dimnames=list(NA,c("X", "Y")), ncol=2)
+
+tsne <- new("Tsne",
+    name=name,
+    pc=pc,
+    perplexity=perplexity,
+    coordinates=coordinates)
+
+test_that("getName works properly", {
+    expect_equal(getName(tsne), name)
+})
+
+test_that("getPerplexity works properly", {
+    expect_equal(getPerplexity(tsne), perplexity)
+})
+
+test_that("getDbscanList works properly", {
+    expect_equal(getPC(tsne), pc)
+})
+
+test_that("getCoordinates works properly", {
+    expect_equivalent(getCoordinates(tsne), coordinates)
+})
+
+
 # ############################### Dbscan getters #################################
-# 
-# test_that("getName works properly", {
-#     expect_equal(getName(dbscan), character(0))
-# })
-# 
-# test_that("getMinPoints works properly", {
-#     expect_equal(getMinPoints(dbscan), numeric(0))
-# })
-# 
-# 
-# test_that("getClustering works properly", {
-#     expect_equivalent(getClustering(dbscan),  matrix(nrow = 0, ncol = 0))
-# })
-# 
-# 
+
+name <- "Clustering_1"
+minPoints <- 1.3
+epsilon <- 3
+clustering <- matrix(data=seq(4), dimnames=list(c("clust.1", "clust.2"),
+                                                c("c1", "c2")),
+                    ncol=2)
+
+
+dbscan <- new("Dbscan",
+                name=name,
+                minPoints=minPoints,
+                epsilon=epsilon,
+                clustering=clustering)
+
+
+test_that("getName works properly", {
+    expect_equal(getName(dbscan), name)
+})
+
+test_that("getMinPoints works properly", {
+    expect_equal(getMinPoints(dbscan), minPoints)
+})
+
+test_that("getEpsilon works properly", {
+    expect_equal(getEpsilon(dbscan), epsilon)
+})
+
+test_that("getClustering works properly", {
+    expect_equivalent(getClustering(dbscan), clustering)
+})
+
+
