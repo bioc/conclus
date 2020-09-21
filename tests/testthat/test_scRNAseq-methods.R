e3a233577f25ba5a8e94a9b02c703b428d03ad7d
Nicolas Descostes <nicolas.descostes@embl.it>
2020-09-21 12:06:57 +0200

    adjust expected message

--- tests/testthat/test_scRNAseq-methods.R
+++ tests/testthat/test_scRNAseq-methods.R
@@ -148,7 +148,7 @@ test_that("Errors are thrown when creating scr", {
             
             expM <- paste0("Not enough cells in the count matrix. ",
                             "There should be at leat 100 cells. The current ",
-                            "count matrix contains 1 cells.")
+                            "count matrix contains 1 cells.\n")
             expect_error(singlecellRNAseq(experimentName  = experimentName, 
                             countMatrix     = matrix(), 
                             species         = "mouse",
@@ -156,7 +156,7 @@ test_that("Errors are thrown when creating scr", {
             
             expM <- paste0("Not enough cells in the count matrix. There should",
                     " be at leat 100 cells. The current count matrix contains",
-                    " 50 cells.")
+                    " 50 cells.\n")
             expect_error(singlecellRNAseq(experimentName  = experimentName, 
                             countMatrix     = smallMatrix, 
                             species         = "mouse",
