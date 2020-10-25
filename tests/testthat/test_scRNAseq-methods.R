6ebec6c76d4e61694b7dc49393a33550329eb413
Nicolas Descostes <nicolas.descostes@embl.it>
2020-10-25 13:26:21 +0100

    add space line 457

--- tests/testthat/test_scRNAseq-methods.R
+++ tests/testthat/test_scRNAseq-methods.R
@@ -454,7 +454,7 @@ test_that("Normalization works properly", {
                 coldata=data.frame()), regexp=expM)
                   
             expM <- paste0("There are no more genes after filtering. Maybe",
-                            "the count matrix contains only genes which are",
+                            " the count matrix contains only genes which are",
                             " less than in 10 cells or more than ",
                             "all-10 cells. Please check the count matrix.")
             expect_error(normaliseCountMatrix(singlecellRNAseq(
