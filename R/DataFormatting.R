a068e2958c1a1e50d65c19123402261c7ff7f072
Nicolas Descostes <nicolas.descostes@embl.it>
2020-10-30 12:19:33 +0100

    build sub-functions

--- R/DataFormatting.R
+++ R/DataFormatting.R
@@ -1,3 +1,181 @@
+#' .retrieveMatrix
+#' 
+#' @description
+#' Download and read a count matrix from a URL
+#' 
+#' @usage
+#' .retrieveMatrix(matrixURL, countMatrixPath)
+#' 
+#' @param matrixURL URL of the count matrix. The matrix must be un-normalized.
+#' @param countMatrixPath Path to the file to which the downloaded count 
+#' matrix will be saved.
+#' 
+#' @return The count matrix as a matrix.
+#' @keywords internal
+#' @noRd
+#' @importFrom utils download.file
+.retrieveMatrix <- function(matrixURL, countMatrixPath){
+    
+    message("Downloading the count matrix.")
+    download.file(matrixURL, countMatrixPath)
+    countMatrix <- as.matrix(read.table(countMatrixPath, header=TRUE, 
+                    row.names=1, stringsAsFactors = FALSE))
+    return(countMatrix)
+    
+}
+
+
+#' .retrieveColMetaData
+#' 
+#' @description
+#' Download the columns meta-data from GEO.
+#' 
+#' @usage
+#' .retrieveColMetaData(seriesMatrixName)
+#' 
+#' @param seriesMatrixName Name of the columns meta-data file hosted on GEO.
+#' This name can usually be found in the 'Series Matrix File(s)' section.
+#'
+#' @details
+#' The method GEOquery::getGEO downloads all the series matrices of the GEO 
+#' record but only the one of interest is kept.
+#' 
+#' @return A data.frame of the columns meta-data.
+#' @keywords internal
+#' @noRd
+#' 
+#' @importFrom stringr str_extract
+#' @importFrom GEOquery getGEO
+#' @importFrom methods as
+.retrieveColMetaData <- function(seriesMatrixName){
+    
+    message("Downloading the columns meta-data.")
+    GEOnb <- stringr::str_extract(seriesMatrixName, "GSE[0-9]+")
+    gpl <- GEOquery::getGEO(GEOnb, GSEMatrix=TRUE)
+    
+    if(!any(names(gpl) == seriesMatrixName))
+        stop("The series matrix was not found. Contact the developper with ",
+                "a reproducible example.")
+    
+    gpl <- gpl[[seriesMatrixName]]
+    gpl <- methods::as(gpl, "data.frame")
+    columnsMetaData <- data.frame(state=gpl$sampletype.ch1, 
+            cellBarcode=gpl$wellbarcode.ch1)
+    
+    return(columnsMetaData)
+}
+
+
+
+#' .filteringAndOrdering
+#' 
+#' @description
+#' Filter and order the count matrix and columns meta-data.
+#' 
+#' @usage
+#' .filteringAndOrdering(countMatrix, columnsMetaData)
+#' 
+#' @param countMatrix Matrix countaining single-cell gene expression retrieved 
+#' from GEO.
+#' @param columnsMetaData Corresponding meta-data of the columns/cells of the 
+#' count matrix.
+#'
+#' @details
+#' The columns of the matrix not having meta-data are removed. The meta-data 
+#' are then re-ordered accordingly to have a match between the matrix columns 
+#' and the rows of the meta-data data.frame. The cells names being "c[0-9]+" 
+#' are added to the matrix and meta-data.
+#' 
+#' @return A list with the first element being the count matrix, and the second 
+#' the columns meta-data.
+#' @keywords internal
+#' @noRd
+.filteringAndOrdering <- function(countMatrix, columnsMetaData){
+    
+    ## Removing columns of the matrix not having meta-data
+    message("Formating data.")
+    idx <- match(colnames(countMatrix), columnsMetaData$cellBarcode)
+    if(isTRUE(all.equal(length(which(!is.na(idx))), 0)))
+        stop("The cell barcodes were not found in the matrix. Are you sure ",
+                "that the count matrix and the series matrix correspond?")
+    
+    idxNA <- which(is.na(idx))
+    if(!isTRUE(all.equal(length(idxNA), 0))){
+        countMatrix <- countMatrix[,-idxNA]
+        idx <- idx[-idxNA]
+    }
+    
+    ## Reordering columns meta-data
+    columnsMetaData <- columnsMetaData[idx,]
+    
+    ## Adding cells names
+    cellsnames <- paste0("c", seq_len(ncol(countMatrix)))
+    colnames(countMatrix) <- cellsnames
+    columnsMetaData <- cbind(cellName=cellsnames, columnsMetaData)
+    rownames(columnsMetaData) <- cellsnames
+    
+    return(list(countMatrix, columnsMetaData))
+    
+}        
+
+
+#' .convertToSymbols
+#' 
+#' @description
+#' Convert the ENSEMBL IDs of the count matrix row names to symbols.
+#' 
+#' @usage
+#' .convertToSymbols(species, countMatrix, annoType)
+#' 
+#' @param species Values should be 'mouse' or 'human'.
+#' @param countMatrix Matrix countaining single-cell gene expression retrieved 
+#' from GEO.
+#' @param annoType Type of the genes annotations contained in the row names of 
+#' the count Matrix. Default: "ENSEMBL".
+#' 
+#' @details
+#' The conversion (TRUE by default) of the row genes IDs (ENSEMBL by default) 
+#' to official genes symbols is done with the function 'bitr' of the 
+#' 'clusterProfiler' package. To see a list of all possible values to pass to 
+#' the annoType parameter use 'keytypes' method on "org.Mm.eg.db" (for mouse) 
+#' or "org.Hs.eg.db" (for human). For example, copy/paste in a R terminal: 
+#' library(org.Mm.eg.db);keytypes(org.Mm.eg.db) 
+#' 
+#' @return The count matrix with row names converted to gene symbols.
+#' @keywords internal
+#' @noRd
+#' @importFrom clusterProfiler bitr
+.convertToSymbols <- function(species, countMatrix, annoType){
+    
+    message("Converting ENSEMBL IDs to symbols.")
+    if(isTRUE(all.equal(species, "mouse")))
+        organismDatabase <- "org.Mm.eg.db"
+    else if(isTRUE(all.equal(species, "human")))
+        organismDatabase <- "org.Hs.eg.db"
+    else
+        stop("species should be mouse or human.")
+    
+    matrixSym <- rownames(countMatrix)
+    symbolsVec <- clusterProfiler::bitr(matrixSym, fromType=annoType, 
+            toType=c("SYMBOL"), OrgDb= organismDatabase)
+    
+    if(isTRUE(is(symbolsVec, "try-error")))
+        stop("The matrix does not contains ENSEMBL IDs. Please verify ",
+                "the row names of the count matrix.")
+    
+    idxSymRemove <- which(duplicated(symbolsVec$ENSEMBL))
+    
+    if(!isTRUE(all.equal(length(idxSymRemove), 0)))
+        symbolsVec <- symbolsVec[-idxSymRemove,]
+    
+    idxSym <- match(symbolsVec$ENSEMBL, matrixSym)
+    rownames(countMatrix)[idxSym] <- symbolsVec$SYMBOL
+    
+    return(countMatrix)
+}
+
+
+
 #' retrieveFromGEO
 #' 
 #' @description
@@ -5,7 +183,7 @@
 #' They are formatted to be suitable inputs for conclus.
 #' 
 #' @usage
-#' retrieveFromGEO <- function(matrixURL, countMatrixPath, seriesMatrixName,
+#' retrieveFromGEO(matrixURL, countMatrixPath, seriesMatrixName,
 #'        species, convertToSymbols=TRUE, annoType="ENSEMBL")
 #' 
 #' @param matrixURL URL of the count matrix. The matrix must be un-normalized.
@@ -53,85 +231,23 @@
 #' @author
 #' Nicolas DESCOSTES
 #' 
-#' @importFrom GEOquery getGEO
-#' @importFrom clusterProfiler bitr
-#' @importFrom stringr str_extract
-#' @importFrom utils download.file
-#' @importFrom methods as
 #' @export retrieveFromGEO
 retrieveFromGEO <- function(matrixURL, countMatrixPath, seriesMatrixName,
         species, convertToSymbols=TRUE, annoType="ENSEMBL"){
     
-    ## Retrieving the count matrix
-    message("Downloading the count matrix.")
-    download.file(matrixURL, countMatrixPath)
-    countMatrix <- as.matrix(read.table(countMatrixPath, header=TRUE, 
-                    row.names=1, stringsAsFactors = FALSE))
-    
-    ## Retrieving the columns meta-data
-    message("Downloading the columns meta-data.")
-    GEOnb <- stringr::str_extract(seriesMatrixName, "GSE[0-9]+")
-    gpl <- GEOquery::getGEO(GEOnb, GSEMatrix=TRUE)
-    
-    if(!any(names(gpl) == seriesMatrixName))
-        stop("The series matrix was not found. Contact the developper with ",
-                "a reproducible example.")
-    
-    gpl <- gpl[[seriesMatrixName]]
-    gpl <- methods::as(gpl, "data.frame")
-    columnsMetaData <- data.frame(state=gpl$sampletype.ch1, 
-            cellBarcode=gpl$wellbarcode.ch1)
-    
-    ## Removing columns of the matrix not having meta-data
-    message("Formating data.")
-    idx <- match(colnames(countMatrix), columnsMetaData$cellBarcode)
-    if(isTRUE(all.equal(length(which(!is.na(idx))), 0)))
-        stop("The cell barcodes were not found in the matrix. Are you sure ",
-                "that the count matrix and the series matrix correspond?")
-    
-    idxNA <- which(is.na(idx))
-    if(!isTRUE(all.equal(length(idxNA), 0))){
-        countMatrix <- countMatrix[,-idxNA]
-        idx <- idx[-idxNA]
-    }
-    
-    ## Reordering columns meta-data
-    columnsMetaData <- columnsMetaData[idx,]
-    
-    ## Adding cells names
-    cellsnames <- paste0("c", seq_len(ncol(countMatrix)))
-    colnames(countMatrix) <- cellsnames
-    columnsMetaData <- cbind(cellName=cellsnames, columnsMetaData)
-    rownames(columnsMetaData) <- cellsnames
-    
+    ## Retrieving the count matrix and columns meta-data
+    countMatrix <- .retrieveMatrix(matrixURL, countMatrixPath)
+    columnsMetaData <- .retrieveColMetaData(seriesMatrixName)
+
+    ## Filtering, ordering and adding cells names
+    result <- .filteringAndOrdering(countMatrix, columnsMetaData)
+    countMatrix <- result[[1]]
+    columnsMetaData <- result[[2]]
+
+
     ## Converting ensembl IDs to gene symbols
-    if(convertToSymbols){
-       
-        message("Converting ENSEMBL IDs to symbols.")
-        if(isTRUE(all.equal(species, "mouse")))
-            organismDatabase <- "org.Mm.eg.db"
-        else if(isTRUE(all.equal(species, "human")))
-            organismDatabase <- "org.Hs.eg.db"
-        else
-            stop("species should be mouse or human.")
-        
-        
-        matrixSym <- rownames(countMatrix)
-        symbolsVec <- clusterProfiler::bitr(matrixSym, fromType=annoType, 
-                toType=c("SYMBOL"), OrgDb= organismDatabase)
-        
-        if(isTRUE(is(symbolsVec, "try-error")))
-            stop("The matrix does not contains ENSEMBL IDs. Please verify ",
-                    "the row names of the count matrix.")
-        
-        idxSymRemove <- which(duplicated(symbolsVec$ENSEMBL))
-        
-        if(!isTRUE(all.equal(length(idxSymRemove), 0)))
-            symbolsVec <- symbolsVec[-idxSymRemove,]
-        
-        idxSym <- match(symbolsVec$ENSEMBL, matrixSym)
-        rownames(countMatrix)[idxSym] <- symbolsVec$SYMBOL
-    }
+    if(convertToSymbols)
+       countMatrix <- .convertToSymbols(species, countMatrix, annoType)
     
     idxDup <- duplicated(rownames(countMatrix))
     ldup <- length(which(idxDup))
@@ -140,7 +256,6 @@ retrieveFromGEO <- function(matrixURL, countMatrixPath, seriesMatrixName,
         countMatrix <- countMatrix[!idxDup,]
         warning("Nb of lines removed due to duplication of row names: ", ldup)
     }
-        
     
     return(list(countMatrix, columnsMetaData))
 }
