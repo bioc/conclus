4ce017b67272cca14d1d61178b57bc16c1b64630
Nicolas Descostes <nicolas.descostes@embl.it>
2020-11-27 14:36:29 +0100

    change variable name line 51

--- R/DataFormatting.R
+++ R/DataFormatting.R
@@ -48,7 +48,7 @@
     download.file(colMetaDataURL, metaDataPath)
     metadata <- read.delim(metaDataPath, header=TRUE, stringsAsFactors=FALSE)
     
-    if(!isTRUE(all.equal(colnames(metaData), c("cellName", "state", 
+    if(!isTRUE(all.equal(colnames(metadata), c("cellName", "state", 
                             "cellBarcode"))))
         warning("The columns of the cells meta-data should be: cellName, ",
                 "state, and cellBarcode. Please correct the dataframe.")
