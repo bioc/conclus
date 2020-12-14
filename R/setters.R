64d5cca84a6b9babb182de6d9d0f09ce81ef07b0
Nicolas Descostes <nicolas.descostes@embl.it>
2020-12-14 11:29:06 +0100

    merged with IlyessSept2020

--- R/setters.R
+++ R/setters.R
@@ -10,19 +10,25 @@
 #' @param value The value to update the slot with. See description or ?scRNAseq.
 #'
 #' @rdname setters
+#' 
 #' @examples
-#' experimentName <- "Bergiers"
-#' countMatrix <- as.matrix(read.delim(system.file(
-#' "extdata/test_countMatrix.tsv", package="conclus")))
-#' outputDirectory <- "YourOutputDirectory"
-#' columnsMetaData <- read.delim(
-#' system.file("extdata/test_colData_filtered.tsv", package="conclus"))
-#'
+#' ## Load the count matrix
+#' countmatrixPath <- system.file("extdata/test_countMatrix.tsv", 
+#'                             package="conclus")
+#' countMatrix <- loadCountMatrix(file=countmatrixPath, header=TRUE, dec=".",
+#'                                 sep='\t')
+#' 
+#' ## Load the coldata
+#' coldataPath <- system.file("extdata/test_colData_filtered.tsv", 
+#'                             package="conclus")
+#' columnsMetaData <- loadColdata(file=coldataPath, columnCell="cell_ID",
+#'                                 header=TRUE, dec=".", sep='\t')
+#' 
 #' ## Create the initial object
-#' scr <- singlecellRNAseq(experimentName = experimentName,
+#' scr <- singlecellRNAseq(experimentName = "Bergiers",
 #'                 countMatrix     = countMatrix,
 #'                 species         = "mouse",
-#'                 outputDirectory = outputDirectory)
+#'                 outputDirectory = "YourOutputDirectory")
 #'                 
 #' setExperimentName(scr) <- "newName"
 #' setCountMatrix(scr) <- countMatrix[seq_len(15), seq_len(150)]
